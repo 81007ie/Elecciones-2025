60787895d31deb3a2e8d6218c5f60d562099b74a
81007ie <81007iemodelo@gmail.com>
2025-11-22 04:04:48 +0000

    actualizacion

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// ⚠️ 1. REEMPLAZA ESTA URL con la de tu ÚLTIMA implementación /exec
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxmYyS1FCVNV5Dv9LGXKt_glMvROk9UkygjxAXBs2bSHv8DzdG7wkm7P-N6ewslptI_Lg/exec'; 
 
 const formulario = document.getElementById('formulario-voto');
